38eeac25b17575e4477bba7282211ae829885d8a
HennaK1 <henna.koli@metropolia.fi>
2022-01-13 18:18:41 +0200

    week1-task1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-/*
-* week 1 examples
-*/
 console.log('Hello console!');
-//const ensisijainen, sit let jos muttuja
 
-let number = 3;
-
-const array = ['a', 'b'];
-array.push('c');
-console.log(array);
-
-number = 4;
