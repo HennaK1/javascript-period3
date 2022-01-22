4ffb897c6aac86a13fbffafee462d9a971425a34
HennaK1 <henna.koli@metropolia.fi>
2022-01-22 13:41:28 +0200

    tehtävän pohja - kesken

--- src/index.js
+++ src/index.js
@@ -1,80 +1,99 @@
-import LunchMenu from './assets/Lunchlist.json';
-// Test
-console.log('lunch menu object', LunchMenu);
+const minValue = 1;
+const maxValue  = 100;
+const maxGuesses = 10;
+let startTime = Date.now();
 
-const coursesEn = [];
-const coursesFi = [];
+let randomNumber = Math.floor(Math.random() * maxValue) + minValue;
 
-for (let i in LunchMenu.courses){
-  coursesFi.push(LunchMenu.courses[i].title_fi);
-  coursesEn.push(LunchMenu.courses[i].title_en);
-}
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const numberRange = document.querySelector('.numberRange');
+const guessDescription = document.querySelector('.guessDescription');
+const time = document.querySelector('.timecount');
 
-const header= document.querySelector('.header');
-const menuName = document.querySelector('.menuName');
-const menu = document.querySelector('.menu');
-const langButton = document.querySelector('.lang');
-const sortButton = document.querySelector('.sort');
-const randomButton = document.querySelector('.random');
-let finnishList = true;
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
 
-//header picture
-const img = document.createElement("img");
-img.src = "./assets/header.jpeg";
-header.appendChild(img);
+let guessCount = 1;
+let resetButton;
+guessField.focus();
 
-coursesFi.forEach(meal => {
-  menuName.textContent = 'Menu suomeksi:';
-  menu.innerHTML += '<li>' + meal + '</li>';
-});
+numberRange.textContent = 'Choose a number between ' + minValue + ' and ' + maxValue + '.';
+guessDescription.textContent = 'See if you can guess it in ' +  maxGuesses + ' turns or fewer.';
 
-//funktio menun vaihto
-const changeLanguage = () =>{
-  menu.innerHTML = '';
-  if (finnishList){
-    coursesEn.forEach(meal => {
-      menuName.textContent = 'Menu englanniksi:';
-      menu.innerHTML += '<li>' + meal + '</li>';
-    });
-    finnishList = false;
-  }else{
-    coursesFi.forEach(meal => {
-      menuName.textContent = 'Menu suomeksi:';
-      menu.innerHTML += '<li>' + meal + '</li>';
-    });
-    finnishList = true;
-  };
-};
-coursesFi.sort();
-coursesEn.sort();
+const checkGuess = () => {
+  const userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' ';
 
-//järjestä menut aakkosittain
-const sortMenu = () => {
-  menu.innerHTML= '';
-  if (finnishList){
-    coursesFi.reverse();
-    coursesFi.forEach(meal => {
-      menu.innerHTML += '<li>' + meal + '</li>';
-    });
-  }else{
-    coursesEn.reverse();
-    coursesEn.forEach(meal => {
-      menu.innerHTML += '<li>' + meal + '</li>';
-    });
-  };
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    stopTimer();
+    setGameOver();
+  } else if (guessCount === maxGuesses) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    lowOrHi.textContent = '';
+    stopTimer();
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'blue';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
 };
 
-//funktio näytä random ateria
-const randomMeal = () => {
-  const random = Math.floor(Math.random()*coursesFi.length);
-  if(finnishList){
-    alert(coursesFi[random]);
-  }else{
-    alert(coursesEn[random]);
+guessSubmit.addEventListener('click', checkGuess);
+
+const stopTimer = () => {
+  const stopTime = Date.now() - startTime;
+
+  if (guessCount === maxGuesses)  {
+    time.textContent += 'Your time was ' + Math.floor(stopTime / 1000) + ' seconds!';
+  }
+  else {
+    time.textContent += 'Your time was ' + Math.floor(stopTime / 1000) + ' seconds! It took ' + guessCount + ' guesses.';
   }
 };
 
-//nappien addeventlistenerit
-langButton.addEventListener('click', changeLanguage);
-sortButton.addEventListener('click', sortMenu);
-randomButton.addEventListener('click', randomMeal);
+const setGameOver = () => {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+};
+
+const resetGame = () => {
+  guessCount = 1;
+  startTime = Date.now();
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (const resetPara of resetParas) {
+    resetPara.textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * maxValue) + minValue;
+};
