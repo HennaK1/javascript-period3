abc57a65bfb0caf5193b5c35e2619b4e58fc66b7
HennaK1 <henna.koli@metropolia.fi>
2022-01-23 20:24:34 +0200

    title changed

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
       },
     ]}),
     new HtmlWebpackPlugin({
-      title: 'Week2-task2',
+      title: 'Lunch',
       meta: {
         viewport: 'width=device-width, initial-scale=1.0'
       },
